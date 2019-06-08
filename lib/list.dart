824f46660a3ed16791696a1bdae7c52569ad4c1f
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2019-06-08 22:52:16 +0530

    To be reverted later

--- lib/list.dart
+++ lib/list.dart
@@ -60,6 +60,123 @@ class _list_ui extends State<List_tab>{
                    ],
                  )
                ),
+               Card(
+                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: <Widget>[
+                       Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Text("Abhimanyu Shekhawat",style: TextStyle(fontWeight:FontWeight.bold,),textAlign: TextAlign.left,),
+                       ),
+                       Padding(
+                         padding: EdgeInsets.all(10),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.start,
+                           children: <Widget>[
+                             Text("Experience:",style: TextStyle(fontWeight:FontWeight.bold,),),
+                             Text("12 Years"),
+                           ],
+                         ),
+                       ),
+                       Padding(
+                         padding: EdgeInsets.all(10) ,
+                         child: Column(
+                           children: <Widget>[
+                             Text("PROFICIENCY"),
+                             Padding(
+                               padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                               child: Column(
+                                 children: <Widget>[
+                                   Text("Flutter"),
+                                   Text("Solidity")
+                                 ],
+                               ),
+                             ),
+                           ],
+                         ),
+                       ),
+                     ],
+                   )
+               ),
+               Card(
+                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: <Widget>[
+                       Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Text("Abhimanyu Shekhawat",style: TextStyle(fontWeight:FontWeight.bold,),textAlign: TextAlign.left,),
+                       ),
+                       Padding(
+                         padding: EdgeInsets.all(10),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.start,
+                           children: <Widget>[
+                             Text("Experience:",style: TextStyle(fontWeight:FontWeight.bold,),),
+                             Text("12 Years"),
+                           ],
+                         ),
+                       ),
+                       Padding(
+                         padding: EdgeInsets.all(10) ,
+                         child: Column(
+                           children: <Widget>[
+                             Text("PROFICIENCY"),
+                             Padding(
+                               padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                               child: Column(
+                                 children: <Widget>[
+                                   Text("Flutter"),
+                                   Text("Solidity")
+                                 ],
+                               ),
+                             ),
+                           ],
+                         ),
+                       ),
+                     ],
+                   )
+               ),
+               Card(
+                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: <Widget>[
+                       Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Text("Abhimanyu Shekhawat",style: TextStyle(fontWeight:FontWeight.bold,),textAlign: TextAlign.left,),
+                       ),
+                       Padding(
+                         padding: EdgeInsets.all(10),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.start,
+                           children: <Widget>[
+                             Text("Experience:",style: TextStyle(fontWeight:FontWeight.bold,),),
+                             Text("12 Years"),
+                           ],
+                         ),
+                       ),
+                       Padding(
+                         padding: EdgeInsets.all(10) ,
+                         child: Column(
+                           children: <Widget>[
+                             Text("PROFICIENCY"),
+                             Padding(
+                               padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                               child: Column(
+                                 children: <Widget>[
+                                   Text("Flutter"),
+                                   Text("Solidity")
+                                 ],
+                               ),
+                             ),
+                           ],
+                         ),
+                       ),
+                     ],
+                   )
+               ),
              ],
 
             )
