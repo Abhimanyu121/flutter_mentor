25044ead41d89853a953126268300a3d7ed1d1a0
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2019-06-29 16:20:03 +0530

    added pageviewer to select topic.

--- lib/addGoals.dart
+++ lib/addGoals.dart
@@ -1,152 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'apiWrapper.dart';
 bool new_topic = false;
 class addGoals extends StatefulWidget{
   _add_goals createState() => new _add_goals();
 }
 class _add_goals extends State<addGoals>{
-  int initial =1;
-  int max =1;
+  static int pos =0;
+  static int state=0;
+  static List list;
+  static List list2;
+ // SharedPreferences prefs;
+  _fetchData() async {
+
+    ApiWrapper wrapper= new ApiWrapper();
+    return wrapper.getTopics();
+
+  }
+  @override
+  void initState() {
+    _fetchData().then((value){
+      setState(() {
+        list = value;
+      });
+    });
+  }
+  //find workaround to get these initialized on call only
+
   @override
   Widget build(BuildContext context) {
-    return new ListView(
+    return (list==null)? _loader():_view();
+  }
+_loader(){
+    return new SpinKitFoldingCube(color: Colors.blue,size: 25.0,);
+}
+_view(){
+  print(list.length);
+    return new Column(
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Card(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Learn Something Cool", style: TextStyle(fontWeight: FontWeight.bold),),
-                DropdownButton<String>(
-                  items: <String>['A', 'B', 'C', 'D'].map((String value) {
-                    return new DropdownMenuItem<String>(
-                      value: null,
-                      child: new Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (_) {},
-                  ),
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
+        Text("CHOOSE A TOPIC",style: TextStyle(fontWeight: FontWeight.bold),),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PageView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, position){
+                pos = position+1;
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(
+                      (position==0)?null: Icons.arrow_back,
+                      color: Colors.black,
                     ),
-                    onPressed: () {
-                     //Navigator.of(context).pushNamed(HomePage.tag);
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.all(5),
-          child: Card(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Teach Something Cool", style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.left,),
-                  (new_topic ?
-                  TextFormField(
-                  keyboardType: TextInputType.text,
-                  autofocus: false,
-                  decoration: InputDecoration(
-                    hintText: 'Name of New Subject',
-                    contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-                  ),
-                  ):DropdownButton<String>(
-                    items: <String>['A', 'B', 'C', 'D'].map((String value) {
-                      return new DropdownMenuItem<String>(
-                        value: null,
-                        child: new Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (_) {},
-                  )
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text("Something new?"),
-                      Checkbox(
-                        value: (new_topic? true:false),
-                        onChanged:_check_change(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 200,
+                        width:250,
+                        child: Card(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                          color: position % 2 == 0 ? Colors.deepOrange : Colors.teal,
+                          child: Center(child: Text(list[position],textAlign: TextAlign.center, style:TextStyle(fontWeight: FontWeight.bold),)),
+                        ),
                       ),
-                    ],
-                  ),
-                  /*Column(
-                    children: iterator(),
-                  ),*/
-                  RaisedButton(
-                    child: Text("Add more"),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        max++;
-                      });
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
-        )
-      ],
-    );
-  }
-  _check_change()=> setState(() {
-      print(new_topic.toString());
-      if (new_topic==true){
-        new_topic=false;
-      }else{
-        new_topic=true;
-      }
-    });
-  dynamic iterator()=> setState(() {
-      int i = initial;
-      List<Widget> arr = new List<Widget>();
-      for(i ; i<=max ; i++){
-        dynamic wid =  adder();
-        arr.add(wid);
-      }
-      return arr;
-    });
-  dynamic adder(){
-    return new Row(
-      children: <Widget>[
-        Container(
-          width: 50,
-          child: TextFormField(
-            autofocus: false,
-            decoration: InputDecoration(
-              hintText: "Time",
+                    Icon(
+                        (position==list.length-1)? null:Icons.arrow_forward
+                    )
+                  ],
+                );
 
-              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+              },
+              physics: BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
             ),
           ),
         ),
-        Container(
-          width: 50,
-          child: TextFormField(
-            autofocus: false,
-            decoration: InputDecoration(
-              hintText: "Task",
-              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        SizedBox(
+          width:200,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              onPressed: (){
+
+              },
+              padding: EdgeInsets.all(12),
+              color: Colors.red,
+              child: Text('Select Topic', style: TextStyle(color: Colors.white)),
             ),
           ),
-        )
+        ),
+
       ],
     );
-  }
+}
+_enroll(){
 
 }
+
+}
+//Text(((state==1 )? list[position%list.length]:list[position%list.length])),
+//position % 2 == 0 ? Colors.deepOrange : Colors.teal,
